60faededda246d41738abc6fe48a554d75002e12
alavanja <aleksandaralavanja@hotmail.com>
2020-07-26 16:17:55 +0200

    pro

--- Nedelja2/Sreda/4.js
+++ Nedelja2/Sreda/4.js
@@ -1,23 +1,23 @@
 let x = 5000;
 for (i = 1; i <= x; i++) {
-    if (i % 5 === 0) {
-        console.log(i);
-    }
+  if (i % 5 === 0) {
+    console.log(i);
+  }
 }
 
 i = 1;
 let y = 5000;
 while (i <= y) {
-    if (i % 5 === 0) {
-        console.log(i);
-    }
-    i++
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+  i++;
 }
-//   2 nacin 
+//   2 nacin
 M = 1;
 while (M <= 5000) {
-    if (M % 5 === 0) {
-        console.log(M);
-    }
-    M++
+  if (M % 5 === 0) {
+    console.log(M);
+  }
+  M++;
 }
